7b02214b09caec97c65250cef64d3c5e33102ca9
Josh Mozes <85561037+JMozes16@users.noreply.github.com>
2023-10-10 17:25:08 -0400

    Inital Project

---
+++ GymHub/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>GymHub</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2A2A2A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
