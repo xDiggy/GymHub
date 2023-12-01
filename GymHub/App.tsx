298dd1a5f4b736a73421bbf19a5ccc5f9ae0d6f3
Antonio Martinez III <toniom4172@gmail.com>
2023-12-01 16:13:36 -0500

    gift if user is logged in

--- GymHub/App.tsx
+++ GymHub/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Alert, Button, StyleSheet, Text, View} from 'react-native';
 import {useAuth0, Auth0Provider} from 'react-native-auth0';
 import config from './auth0-configuration';
+import { Image } from 'react-native';
 
 const Home = () => {
   const {authorize, clearSession, user, getCredentials, error, isLoading} = useAuth0();
@@ -22,15 +23,20 @@ const Home = () => {
     return <View style={styles.container}><Text>Loading</Text></View>;
   }
 
+  
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}> Auth0Sample - Login </Text>
       {user && <Text>You are logged in as {user.name}</Text>}
       {!user && <Text>You are not logged in</Text>}
-      <Button
-        onPress={isLoggedIn ? handleLogout : handleLogin}
-        title={isLoggedIn ? 'Log Out' : 'Log In'}
-      />
+      {user && <Image source={{uri: 'https://cataas.com/cat'}} style={{width: 200, height: 200}} />}
+      {!user && 
+        <Button
+          onPress={handleLogin}
+          title='Log In'
+        />
+      }
       {error && <Text style={styles.error}>{error.message}</Text>}
     </View>
   );
