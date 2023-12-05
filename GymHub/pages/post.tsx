2932021b1f0de272c20b0f89fc3977982f450c6d
Antonio Martinez III <toniom4172@gmail.com>
2023-12-05 16:30:14 -0500

    added more details include name, weight, and reps

--- GymHub/pages/post.tsx
+++ GymHub/pages/post.tsx
@@ -1,17 +1,55 @@
 // More detailed boilerplate workout log page with workout details
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const WorkoutLog = () => {
+  const [workoutsDone, setWorkoutsDone] = React.useState(0);
+  const [workoutName, setWorkoutName] = React.useState('');
+  const [workoutWeight, setWorkoutWeight] = React.useState('');
+  const [workoutReps, setWorkoutReps] = React.useState('');
+
+  const renderWorkouts = () => {
+    const workoutList = [];
+    for (let i = 0; i < workoutsDone; i++) {
+      workoutList.push(
+        <View key={i}>
+          <Text>Workout {i + 1}</Text>
+          <TextInput
+            value={workoutName}
+            onChangeText={(text) => setWorkoutName(text)}
+            placeholder="Workout Name"
+          />
+          <TextInput
+            value={workoutWeight}
+            onChangeText={(text) => setWorkoutWeight(text)}
+            keyboardType="numeric"
+            placeholder="Weight (lbs)"
+          />
+          <TextInput
+            value={workoutReps}
+            onChangeText={(text) => setWorkoutReps(text)}
+            keyboardType="numeric"
+            placeholder="Reps"
+          />
+        </View>
+      );
+    }
+    return workoutList;
+  };
+
   return (
     <View style={styles.container}>
       <Text>Welcome to your workout log!</Text>
       <Text>Track your progress here.</Text>
       <View>
-        <Text>Workouts Done: 5</Text>
-        <Text>Max Weight: 100 lbs</Text>
+        <TextInput
+          value={workoutsDone.toString()}
+          onChangeText={(text) => setWorkoutsDone(parseInt(text) || 0)}
+          keyboardType="numeric"
+          placeholder="Workouts Done"
+        />
+        {renderWorkouts()}
       </View>
-      {/* Add workout log components here */}
     </View>
   );
 };
@@ -25,5 +63,3 @@ const styles = StyleSheet.create({
 });
 
 export default WorkoutLog;
-
-
